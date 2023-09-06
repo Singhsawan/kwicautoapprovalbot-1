344db4bcc98cc41449d2fef86505b51c68e574c4
Jack <125857863+Singhsawan@users.noreply.github.com>
2023-09-06 10:33:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "21748181"))
     API_HASH = getenv("API_HASH", "b1d962414e186e0778911f3183feac33")
     BOT_TOKEN = getenv("BOT_TOKEN", "6160088499:AAFN8uclac9l2GPJwLeTRdupPLcR7TbDQTU")
-    FSUB = getenv("FSUB", "New_Moviee_hindi")
-    CHID = int(getenv("CHID", "-1001952663939"))
+    FSUB = getenv("FSUB", "i_Movieee")
+    CHID = int(getenv("CHID", "--100184890614"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Sawansingh24:Sawansingh24@cluster0.uiuhxxj.mongodb.net/?retryWrites=true&w=majority")
     
